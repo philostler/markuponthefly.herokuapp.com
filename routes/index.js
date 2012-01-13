a67721a2e7f0f5c28c505b24d10c54545adddf04
Phil Ostler <philostler@gmail.com>
2012-01-13 00:43:03 +0000

    Output rendered markdown

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
-
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render("index")
+};
+
+exports.render = function(request, response){
+  var input = request.body.raw;
+  var output = require("markdown").markdown.toHTML(input);
+  response.end(output);
 };
